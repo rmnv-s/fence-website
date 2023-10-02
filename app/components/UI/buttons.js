0e9a62ae036f0b2076a28989dc466eaa5ba9c7aa
dev <remnev.dev@gmail.com>
2023-10-02 17:54:40 +0300

    feat: discount

--- app/components/UI/buttons.js
+++ app/components/UI/buttons.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Button = ({ onClick, className, text }) => {
-  return (
-    <button className={`bg-yellow uppercase ${className}`} onClick={onClick}>
-      {text}
-    </button>
-  );
+    return (
+        <button className={` uppercase ${className}`} onClick={onClick}>
+            {text}
+        </button>
+    );
 };
 
 export default Button;
--- app/components/headerContant/headerContant.js
+++ app/components/headerContant/headerContant.js
@@ -28,7 +28,13 @@ const HeaderContent = () => {
                 />
 
                 <div className="flex items-center justify-start h-full max-w-100  m-auto px-10">
-                    <div className="relative mb-60 max-[560px]:mb-0 mt-[150px]">
+                    <div className="relative mb-60 max-[560px]:mb-0 mt-[80px]">
+                        <div className="flex justify-between items-center text-[#161616] text-l leading-[3.2rem] max-w-[49rem] mb-20 max-[320px]:text-[1.8rem] uppercase p-12 bg-yellow font-bold max-[425px]:flex-col">
+                            <span className="text-[6.3rem] max-[425px]:mb-8"> - 5% </span>
+                            <p className="text-center">
+                                При заказе забора <br /> до 15 октября
+                            </p>
+                        </div>
                         <h1 className="font-bold text-whiteFont leading-[8.5rem] max-w-[76.8rem] mb-16 max-[320px]:text-[3.8rem] max-[320px]:leading-[5rem] max-[525px]:text-[6rem] max-[525px]:leading-[6rem]  text-[8.4rem] leading-[9rem] mb-6">
                             Установка заборов под ключ
                         </h1>
@@ -37,8 +43,8 @@ const HeaderContent = () => {
                         </p>
                         <Button
                             onClick={handleButtonClick}
-                            className="px-10 py-5 text-sm"
-                            text="Заказать звонок"
+                            className="px-20 py-5 text-m uppercase mt-20 border border-yellow text-whiteFont transition-all duration-500 ease-in-out tracking-wider hover:text-black hover:bg-yellow"
+                            text="Заказать"
                         />
                         {isPopupVisible && <Popup onClose={handleClosePopup} />}
                     </div>
